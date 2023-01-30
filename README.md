9125033b394e462507c964ccbf2d28f84e30510e
Collin <collinm@fullstackacademy.com>
2023-01-30 11:41:45 -0600

    Homework downloader initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# homework-downloader
+# homework-downloader
---
+++ download-homework-1.js
@@ -0,0 +1,24 @@
+import csv2json from 'csvtojson';
+import { exec } from 'child_process';
+import { promisify } from 'util';
+
+const execPromise = promisify(exec);
+
+const submissions = await csv2json().fromFile('./submissions.csv')
+for (const submission of submissions) {
+  const name = submission.Name;
+  const project = submission['Please select the project you are submitting'];
+  const url = submission['URL to Deployed Site (Netlify)'];
+
+  const projectDir = `"./submission-content/${name}/${project}"`;
+  console.log(`Downloading submission ${name}: ${url}`)
+  try {
+    await execPromise(`mkdir -p ${projectDir}`);
+    await execPromise(`wget --no-parent --recursive --directory-prefix ${projectDir} ${url}`);
+    console.log("Download successful")
+  }
+  catch (error) {
+    console.error("Failed to download")
+    console.error(error);
+  }
+}
